c647ee5a14bff07d2cffa4786048b7e16ab697c5
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-06-19 00:33:22 +0100

    Just few additions, still cooking

---
+++ Outline/00.Foundation/09.Logical-and-comparison-operators/LogicalAndComparisonOperators.js
@@ -0,0 +1,25 @@
+console.log("Comparison Operators")
+console.log(10 == 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 != 10);
+console.log(10 != 11);
+
+console.log("Logical Operators")
+// Both conditions have to be true for the answer to return as true
+console.log(10 == 10 && 20 == 20 && 100 == 100);
+console.log(10 == 10 && 20 != 20);
+
+// At least one condition has to be true to return true
+console.log(20 == 30 || 10 == 10);
+
+console.log(!(10 == 10));
+console.log(!(10 == 20));
+
+if ((10 == 20)) {
+    console.log("if statement was executed");
+}
+
+console.log("hello" == "hola")
