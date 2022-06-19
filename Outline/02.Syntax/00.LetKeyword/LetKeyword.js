720d93db675a140846d4047767997e34443b9a4b
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-06-19 13:23:21 +0100

    Still cooking

---
+++ Outline/02.Syntax/00.LetKeyword/LetKeyword.js
@@ -0,0 +1,8 @@
+// COPYRIGHT © 2022 Alao Abdul-Quayyum, All rights Reserved.
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
---
+++ Outline/02.Syntax/01.ConstKeyword/ConstKeyword.js
@@ -0,0 +1,14 @@
+// COPYRIGHT © 2022 Alao Abdul-Quayyum, All rights Reserved.
+
+
+
+const person = {}
+
+person['name'] = "Abdul-Quayyum"
+
+console.log(person.name)
+
+const array = []
+array.push("Abdul-Quayyum from Kwara State")
+
+console.log(array[0]) 
