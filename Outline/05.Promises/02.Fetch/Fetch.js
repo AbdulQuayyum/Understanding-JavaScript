584757e0b02360a9eea4147dc8d70e97f6279685
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-06-20 22:14:49 +0100

    Still cooking

--- Outline/05.Promises/02.Fetch/Fetch.js
+++ Outline/05.Promises/02.Fetch/Fetch.js
@@ -1,1 +1,18 @@
 // COPYRIGHT © 2022 Alao Abdul-Quayyum, All rights Reserved.
+
+// The Fetch API provides a JavaScript interface for accessing and manipulating parts of the HTTP pipeline, such as requests and responses. It also provides a global fetch() method that provides an easy, logical way to fetch resources asynchronously across the network.
+
+const getRandomUsers = n => {
+    const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`)
+    fetchRandomUsers.then(data => {
+        data.json().then(randomUsers => {
+            console.log(JSON.stringify(randomUsers.results.length));
+            randomUsers.results.forEach(user => {
+                const { gender, email } = user;
+                console.log(`${gender} - ${email}`);
+            });
+        })
+    });
+}
+
+getRandomUsers(100);
